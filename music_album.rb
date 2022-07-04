3604de4ae9c743e006969cc29a8a304b1568c02d
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-07-04 19:34:10 +0300

    added super call for the parent class

--- music_album.rb
+++ music_album.rb
@@ -1,7 +1,9 @@
-require "./item"
+require './item'
 class MusicAlbum < item
-  attr_accessor :on_spotify
-  def initialize(on_spotify:)
+  attr_accessor :publish_date, :on_spotify, :archived
+
+  def initialize(publish_date:, archived: false)
+    super(publish_date: publish_date, archived: archived)
     @on_spotify = on_spotify
   end
 end
