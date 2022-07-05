73ffb3d07b74aa20155f4d1061cfc791042dc69a
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-07-05 17:31:39 +0200

    add game table

--- schema.sql
+++ schema.sql
@@ -21,3 +21,18 @@ CREATE TABLE Genre(
 CREATE TABLE Music_Album(
     on_spotify BOOLEAN
     ) INHERITS (Item)
+
+CREATE TABLE games(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  multiplayer BOOLEAN,
+  last_played_at DATE,
+  FOREIGN KEY(id) REFERENCES item(id)
+);
+
+CREATE TABLE author(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  PRIMARY KEY(id)
+);
+
