63b764dbb63acb534a81656adb036cb1669358cb
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-07-05 13:31:34 +0200

    add id to facilitate retrieving data from json file

--- author.rb
+++ author.rb
@@ -1,11 +1,11 @@
 class Author
   attr_accessor :first_name, :last_name, :id, :items
 
-  def initialize(first_name:, last_name:)
+  def initialize(id:nil,first_name:, last_name:)
+    @id = Random.rand(1..1000)
     @first_name = first_name
     @last_name = last_name
     @items = []
-    @id = Random.rand(1..1000)
   end
 
   def add_item(item)
