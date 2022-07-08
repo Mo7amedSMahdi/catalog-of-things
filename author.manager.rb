887bbd35f6df2ed2538f45210af7c59db39d6067
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-07-08 11:32:01 +0200

    fix rubocop offenses

--- author.manager.rb
+++ author.manager.rb
@@ -2,9 +2,11 @@ require 'json'
 require_relative 'author'
 class AuthorManager
   attr_accessor :authors
+
   def initialize
     @authors = []
   end
+
   def add_author
     puts '####################################'
     puts '###### Creating a New Author #######'
@@ -13,20 +15,22 @@ class AuthorManager
     first_name = gets.chomp
     puts 'Last name'
     last_name = gets.chomp
-    author = Author.new(first_name:first_name, last_name:last_name)
+    author = Author.new(first_name: first_name, last_name: last_name)
     @authors.push(author) unless @authors.include?(author)
     author
   end
+
   def list_authors
     @authors.each_with_index do |author, index|
       puts "#{index}) #{author.to_json}"
     end
   end
+
   def store_authors
     File.write('data/author.json', JSON.pretty_generate(@authors), mode: 'w')
   end
+
   def load
     JSON.parse(File.read('data/author.json')) if File.exist?('data/author.json')
   end
 end
-
